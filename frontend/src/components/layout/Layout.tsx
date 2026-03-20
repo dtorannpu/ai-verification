ffb9fc88b1fcda5292306870de4692a3c4e23df1
dtorannpu <dtorannpu@gmail.com>
2026-03-20 09:21:07 +0900

    feat: add Layout component and update routing in main entry point; configure path aliases in tsconfig

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router";
 
 export default function Layout() {
   return (
-    <>
+    <main>
       <Outlet />
-    </>
+    </main>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
-import "./index.css";
-import Home from "./pages/Home";
-import Layout from "./components/layout";
+import "@/index.css";
+import Home from "@/pages/Home";
+import Layout from "@/components/layout/Layout";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
