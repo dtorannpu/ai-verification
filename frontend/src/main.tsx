af5d53bc96d80b04e0941f83a0972cfd0c2a2cfe
dtorannpu <dtorannpu@gmail.com>
2026-03-20 09:06:40 +0900

    feat: implement layout and home page components, update main entry point

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
-import App from "./App.tsx";
+import Home from "./pages/Home";
+import Layout from "./components/layout";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
